92d8b4ed6b300491e8b266a11e6a94bd2f576b80
Sami Kankaristo <sami@indiumgames.fi>
2015-03-07 17:50:48 +0200

    Parsing for docstrings, signatures and commands
    The actual parsing is working really well so far, and it's pretty fast
    as well.
    
    However, objects aren't being created yet and commands aren't run, even
    though they are parsed from the docstrings.

--- hactdoc.lua
+++ hactdoc.lua
@@ -6,6 +6,9 @@
 --!
 
 
+--!
+--! HactDoc module.
+--!
 local HactDoc = {
     Cpp = require("hactdoc_cpp");
     --Lua = {};
@@ -20,17 +23,22 @@ local HactDoc = {
 --! :param file:  The source file to parse
 --!
 function HactDoc.ParseFile(file)
-    print("*******************************************************************")
+    print()
+    --print("*******************************************************************")
     print("Parsing file: ", file)
-    print("===================================================================")
+    local start = os.clock()
+    --print("===================================================================")
     
     if file:match("%.h$") or file:match("%.cpp$") then
-        HactDoc.Cpp.ParseFile(file)
+        HactDoc.Cpp.ParseFile(file, HactDoc.objects)
     elseif file:match("%.lua$") then
         print("Lua currently unsupported...")
     end
     
-    print("*******************************************************************")
+    --print("===================================================================")
+    print("    Parsing done, took: " .. (os.clock() - start) .. " s")
+    --print("*******************************************************************")
+    print()
 end
 
 
@@ -48,15 +56,71 @@ function HactDoc.HactDoc(sourceFiles, outputDir)
         error(2, "No source files specified")
     end
     
+    local start = os.clock()
+    
     for _, sourceFile in ipairs(sourceFiles) do
         HactDoc.ParseFile(sourceFile)
     end
     
+    print()
+    print("Parsed all files, took: " .. (os.clock() - start) .. " s")
     print("Saving output to directory: ", outputDir)
 end
 
 
 HactDoc.HactDoc{
+    "../HactEngine/src/audio.h";
+    "../HactEngine/src/audio.cpp";
+    "../HactEngine/src/audiomanager.h";
+    "../HactEngine/src/audiomanager.cpp";
     "../HactEngine/src/chronotime.h";
     "../HactEngine/src/chronotime.cpp";
+    "../HactEngine/src/container.h";
+    "../HactEngine/src/container.cpp";
+    "../HactEngine/src/debug.h";
+    "../HactEngine/src/debug.cpp";
+    "../HactEngine/src/editor.h";
+    "../HactEngine/src/editor.cpp";
+    "../HactEngine/src/editorwindow.h";
+    "../HactEngine/src/editorwindow.cpp";
+    "../HactEngine/src/entity.h";
+    "../HactEngine/src/entity.cpp";
+    "../HactEngine/src/gameengine.h";
+    "../HactEngine/src/gameengine.cpp";
+    "../HactEngine/src/hierarchy.h";
+    "../HactEngine/src/hierarchy.cpp";
+    "../HactEngine/src/input.h";
+    "../HactEngine/src/input.cpp";
+    "../HactEngine/src/logger.h";
+    "../HactEngine/src/logger.cpp";
+    "../HactEngine/src/loglistener.h";
+    "../HactEngine/src/loglistener.cpp";
+    "../HactEngine/src/logmessage.h";
+    "../HactEngine/src/logqueue.h";
+    "../HactEngine/src/logqueue.cpp";
+    "../HactEngine/src/main.cpp";
+    "../HactEngine/src/mesh.h";
+    "../HactEngine/src/mesh.cpp";
+    "../HactEngine/src/property.h";
+    "../HactEngine/src/property.cpp";
+    "../HactEngine/src/resourcemanager.h";
+    "../HactEngine/src/resourcemanager.cpp";
+    "../HactEngine/src/resourceutil.h";
+    "../HactEngine/src/resourceutil.cpp";
+    "../HactEngine/src/scriptingengine.h";
+    "../HactEngine/src/scriptingengine.cpp";
+    "../HactEngine/src/shaderprogram.h";
+    "../HactEngine/src/shaderprogram.cpp";
+    "../HactEngine/src/text.h";
+    "../HactEngine/src/text.cpp";
+    "../HactEngine/src/texture.h";
+    "../HactEngine/src/texture.cpp";
+    "../HactEngine/src/thread.h";
+    "../HactEngine/src/thread.cpp";
+    "../HactEngine/src/util.h";
+    "../HactEngine/src/util.cpp";
+    "../HactEngine/src/xmlelement.h";
+    "../HactEngine/src/xmlelement.cpp";
+    "../HactEngine/src/xmlutils.h";
+    "../HactEngine/src/xmlutils.cpp";
 }
--- hactdoc_cpp.lua
+++ hactdoc_cpp.lua
@@ -10,13 +10,98 @@ local Cpp = {}
 
 
 --!
---! Strip C++ docstring.
+--! Test if a string is the beginning of a docstring.
+--!
+--! :param str:  The string to test.
+--!
+--! :returns:  True, if the string is the beginning of a docstring.
+--!
+local function IsDocstringBeginning(str)
+    return (str:match("^%s*/%*!") or str:match("^%s*//!"))
+end
+
+
+--!
+--! Parse docstring commands.
+--!
+--! :param commands:  The command string to parse.
+--!
+local function ParseDocstringCommands(commands)
+    print("Commands: ", commands)
+end
+
+
+--!
+--! Collects a docstring.
+--!
+--! :param lines:       Lines in a file.
+--! :param lineNumber:  Line to begin on.
+--!
+--! :returns:  1. The line where the docstring ends.
+--!            2. The collected docstring.
+--!
+local function CollectDocstring(lines, lineNumber)
+    -- First line of the docstring
+    local firstLine = lines[lineNumber]
+    
+    -- Check for multiline comment
+    local multilineComment = (firstLine:find("/%*!") or false)
+    
+    -- Get commands from the first line
+    local commands = (firstLine:match("%[[^%]]*%]") or "")
+    commands = commands .. (firstLine:match("[<>^~]+") or "")
+    
+    -- Parse docstring commands
+    ParseDocstringCommands(commands)
+    
+    -- Strip the commands from the first line
+    lines[lineNumber] = lines[lineNumber]:gsub("![^%s%*]*", "!")
+    
+    -- Start collecting docstring
+    local docstring = ""
+    
+    -- Iterate over lines
+    while lineNumber <= #lines do
+        -- Current line
+        local line = lines[lineNumber]
+        
+        if multilineComment then
+            if not line:match("%*/") then
+                -- Append the line to the docstring
+                docstring = docstring .. line .. "\n"
+            else
+                -- Append the last line to the docstring
+                docstring = docstring .. line
+                
+                lineNumber = lineNumber + 1
+                break
+            end
+        else
+            if line:match("^%s*//") then
+                -- Append the line to the docstring
+                docstring = docstring .. line .. "\n"
+            else
+                lineNumber = lineNumber + 1
+                break
+            end
+        end
+        
+        -- Go to the next line
+        lineNumber = lineNumber + 1
+    end
+    
+    return lineNumber, docstring
+end
+
+
+--!
+--! Strip the docstring.
 --!
 --! :param docstring:  Docstring to strip (single or multiple lines).
 --!
 --! :returns:  The stripped docstring.
 --!
-function Cpp.StripDocstring(docstring)
+local function StripDocstring(docstring)
     --[[
     print("Docstring:")
     print("```")
@@ -25,19 +110,38 @@ function Cpp.StripDocstring(docstring)
     print()
     --]]
     
+    --[[
+    print(":vvv")
+    print(docstring)
+    print(":^^^")
+    --]]
+    
+    -- Remove the docstring commands
+    docstring = docstring:gsub("![^%s%*]*", "!", 1)
+    
+    --[[
+    print("=vvv")
+    print(docstring)
+    print("=^^^")
+    --]]
+    
+    -- The stripped docstring
     local strippedDocstring = ""
     
-    for line in docstring:gmatch("[^\r\n]+") do
-        --print(">>", line)
-        
-        line = line:gsub("^%s*/%*%s?", "")
-        line = line:gsub("^%s*%*/", "")
-        line = line:gsub("^%s*%*%s?", "")
+    -- Iterate over the lines in the docstring
+    for line in docstring:gmatch("[^\n]+") do
+        -- Strip beginning of the line
+        line = line:gsub("^%s*//!?%s?", "")
+        line = line:gsub("^%s*/?%*[!/]?%s?", "")
         
         strippedDocstring = strippedDocstring .. line .. "\n"
     end
     
-    --[[
+    -- Strip whitespace and line changes at the start and end of the docstring
+    strippedDocstring = strippedDocstring:gsub("^[%s\n]+", "")
+    strippedDocstring = strippedDocstring:gsub("[%s\n]+$", "")
+    
+    ---[[
     print("Stripped docstring:")
     print("```")
     print(strippedDocstring)
@@ -50,27 +154,104 @@ end
 
 
 --!
+--! Collect signature.
+--!
+--! :param lines:       Lines in a file.
+--! :param lineNumber:  Line to begin on.
+--!
+--! :returns:  1. The line where the signature ends.
+--!            2. The collected signature.
+--!
+local function CollectSignature(lines, lineNumber)
+    -- The signature
+    local signature = ""
+    
+    -- Count indentation characters on first line
+    local indentSize = lines[lineNumber]:find("[^%s]") or 1
+    
+    -- Iterate over lines
+    while lineNumber <= #lines do
+        -- Current line
+        local line = lines[lineNumber]
+        
+        -- Search for a '{' or a ';'
+        local signatureEnd = line:find("%s*[{;]")
+        
+        if not signatureEnd then
+            signature = signature .. line:sub(indentSize) .. " \\\n"
+        else
+            -- Hit the end of the signature
+            signature = signature .. line:sub(indentSize, signatureEnd - 1)
+            
+            lineNumber = lineNumber + 1
+            break
+        end
+        
+        -- Go to the next line
+        lineNumber = lineNumber + 1
+    end
+    
+    print("Signature:")
+    print("```")
+    print(signature)
+    print("```")
+    print()
+    
+    return lineNumber, signature
+end
+
+
+--!
 --! Parse C++ file.
 --!
 --! :param file:  C++ source file to parse.
 --!
 function Cpp.ParseFile(file)
-    local docstring = ""
+    local docstring = nil
+    local signature = nil
     
+    -- Read the file into a table
+    local lines = {}
     for line in io.lines(file) do
-        print(">", line)
+        lines[#lines + 1] = line
+    end
+    
+    -- Iterate over lines
+    local lineNumber = 1
+    while lineNumber <= #lines do
+        -- Current line
+        local line = lines[lineNumber]
         
-        if line:match("^%s*/%*!") or line:match("^%s*/%*%*") or line:match("^%s*%*") then
-            docstring = docstring .. line .. "\n"
-        end
+        --print(">", line)
         
-        if line:match("%*/$") then
-            Cpp.StripDocstring(docstring)
-            docstring = ""
+        if IsDocstringBeginning(line) then
+            print("Docstring beginning at line: ", lineNumber)
+            
+            -- Found beginning of docstring, collect docstring
+            lineNumber, docstring = CollectDocstring(lines, lineNumber)
+            
+            -- Save the full docstring
+            --...
+            
+            -- Strip the docstring
+            docstring = StripDocstring(docstring)
+            
+            -- Save the stripped docstring
+            --...
+            
+            print("Signature beginning at line:", lineNumber)
+            
+            -- Collect signature
+            lineNumber, signature = CollectSignature(lines, lineNumber)
+            
+            -- Run docstring commands
+            --...
+        else
+            -- Go to next line
+            lineNumber = lineNumber + 1
         end
     end
 end
 
 
 return Cpp
-
